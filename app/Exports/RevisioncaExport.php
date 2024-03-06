ac06637e6feaee22308eb2200f790acf897e35ed
aforoeepp <aforoeeppelsantuario@gmail.com>
2024-03-05 19:58:52 -0500

    modificar

--- app/Exports/RevisioncaExport.php
+++ app/Exports/RevisioncaExport.php
@@ -11,15 +11,7 @@ class RevisioncaExport implements FromView
 {
     public function view(): View
     {
-        // $revisionca = Revisionca::selectRaw("id, 
-        //                         codigo , 
-        //                         nombre, 
-        //                         direccion, 
-        //                         CASE estado
-        //                         WHEN '0' THEN 'En revisión'
-        //                         WHEN '1' THEN 'Aplica'
-        //                         ELSE 'No aplica' END AS estado, observacion")->whereIn('estado', ['1'])
-        //                         ->where('ruta', $request->ruta)->orderBy('updated_at', 'desc')->get();Revisionca::all()
+        //Revisionca::all()
         return view('revisionca.exports.invoices', [
             'invoices' => Revisionca::selectRaw("id,ruta,codigo,lecturaan,consumoan,lecturaac,consumoac, 
             promedio , causadenol,nombre,estrato,direccion,nmedidor,                                  
